ab29a42bee68e39eba0d0bc45384d92c25921bad
Nobuo Yamashita <nobsun@sampou.org>
2025-11-08 03:01:28 +0900

    catch up

---
+++ app/abc001c/Main.hs
@@ -0,0 +1,247 @@
+{-# LANGUAGE GHC2021 #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LexicalNegation #-}
+{-# LANGUAGE LambdaCase, MultiWayIf #-}
+{-# LANGUAGE NPlusKPatterns #-}
+{-# LANGUAGE DataKinds, PolyKinds, NoStarIsType, TypeFamilyDependencies #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedRecordDot, NoFieldSelectors, DuplicateRecordFields #-}
+module Main where
+
+import Data.ByteString.Char8 qualified as B
+import Data.Maybe
+import Data.Ord
+
+import Control.Arrow
+import Control.Applicative
+import Data.Array
+import Data.Bool
+import Data.Char
+import Data.Function
+import Data.List
+import Text.Printf
+
+import Data.IntMap qualified as IM
+import Data.IntSet qualified as IS
+import Data.Map qualified as M
+import Data.Set qualified as S
+import Data.Vector qualified as V
+
+import Debug.Trace qualified as Debug
+
+debug :: Bool
+debug = () /= ()
+
+type I = Double
+type O = String
+
+type Solver = (I,I) -> [O]
+
+solve :: Solver
+solve = \ case
+    (deg,dis) -> [dir, w]
+        where
+            dir = bool "C" (toDir deg) (w /= "0")
+            w   = toW dis
+
+toDir :: Double -> String
+toDir deg = if
+    | d < 11.25 -> "N"
+    | d < 33.75 -> "NNE"
+    | d < 56.25 -> "NE"
+    | d < 78.75 -> "ENE"
+    | d < 101.25 -> "E"
+    | d < 123.75 -> "ESE"
+    | d < 146.25 -> "SE"
+    | d < 168.75 -> "SSE"
+    | d < 191.25 -> "S"
+    | d < 213.75 -> "SSW"
+    | d < 236.25 -> "SW"
+    | d < 258.75 -> "WSW"
+    | d < 281.25 -> "W"
+    | d < 303.75 -> "WNW"
+    | d < 326.25 -> "NW"
+    | d < 348.75 -> "NNW"
+    | otherwise  -> "N"
+    where
+        d = deg / 10
+
+toW :: Double -> String
+toW dis = if
+    | d < 0.25 -> "0"
+    | d < 1.55 -> "1"
+    | d < 3.35 -> "2"
+    | d < 5.45 -> "3"
+    | d < 7.95 -> "4"
+    | d < 10.75 -> "5"
+    | d < 13.85 -> "6"
+    | d < 17.15 -> "7"
+    | d < 20.75 -> "8"
+    | d < 24.45 -> "9"
+    | d < 28.45 -> "10"
+    | d < 32.65 -> "11"
+    | otherwise -> "12"
+    where
+        d = dis / 60
+
+wrap :: Solver -> ([[I]] -> [[O]])
+wrap f = \ case
+    [des,deg]:_ -> case f (des,deg) of
+        rr -> [rr]
+    _   -> error "wrap: invalid input format"
+
+main :: IO ()
+main = B.interact (encode . wrap solve . decode)
+
+class InterfaceForOJS a where
+    readB :: B.ByteString -> a
+    readBs :: B.ByteString -> [a]
+    readBs = map readB . B.words
+    decode :: B.ByteString -> [[a]]
+    decode = map readBs . B.lines
+
+    showB :: a -> B.ByteString
+    showBs :: [a] -> B.ByteString
+    showBs = B.unwords . map showB
+    encode :: [[a]] -> B.ByteString
+    encode = B.unlines . map showBs
+
+instance InterfaceForOJS Int where
+    readB = readInt
+    showB = showInt
+
+instance InterfaceForOJS String where
+    readB = readStr
+    showB = showStr
+
+instance InterfaceForOJS Double where
+    readB = readDbl
+    showB = showDbl
+
+instance InterfaceForOJS Char where
+    readB = B.head
+    showB = B.singleton
+    readBs = B.unpack
+    showBs = B.pack
+
+readInt :: B.ByteString -> Int
+readInt = fst . fromJust . B.readInt
+
+showInt :: Int -> B.ByteString
+showInt = B.pack . show
+
+readStr :: B.ByteString -> String
+readStr = B.unpack
+
+showStr :: String -> B.ByteString
+showStr = B.pack
+
+readDbl :: B.ByteString -> Double
+readDbl = read . B.unpack
+
+showDbl :: Double -> B.ByteString
+showDbl = B.pack . show
+
+{- Bonsai -}
+
+{- |
+>>> combinations 2 "abcd"
+["ab","ac","ad","bc","bd","cd"]
+-}
+combinations :: Int -> [a] -> [[a]]
+combinations = \ case
+    0   -> const [[]]
+    n+1 -> \ case 
+        []   -> []
+        x:xs -> map (x:) (combinations n xs) ++ combinations (n+1) xs
+    _ -> error "negative"
+
+{- |
+>>> spanCount odd [3,1,4,1,5,9]
+(2,[4,1,5,9])
+-}
+spanCount :: (a -> Bool) -> [a] -> (Int, [a])
+spanCount p = \ case
+    []   -> (0,[])
+    aas@(a:as)
+        | p a       -> case spanCount p as of
+            (c,bs)      -> (succ c, bs)
+        | otherwise -> (0,aas)
+
+{- |
+>>> runLength "aaaabbbcddeeeeeefghhhh"
+[('a',4),('b',3),('c',1),('d',2),('e',6),('f',1),('g',1),('h',4)]
+-}
+runLength :: Eq a => [a] -> [(a, Int)]
+runLength = runLengthBy (==)
+
+runLengthBy :: (a -> a -> Bool) -> [a] -> [(a, Int)]
+runLengthBy eq = unfoldr phi
+  where
+    phi []     = Nothing
+    phi (x:xs) = case spanCount (x `eq`) xs of
+      (m, zs) -> Just ((x, succ m) , zs)
+
+{- |
+>>> splitEvery 3 [0 .. 10]
+[[0,1,2],[3,4,5],[6,7,8],[9,10]]
+-}
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery k = \ case
+    [] -> []
+    xs -> case splitAt k xs of
+        (ys,zs) -> ys : splitEvery k zs
+
+{- |
+>>> subsegments "yay"
+[["y","a","y"],["ya","ay"],["yay"]]
+-}
+subsegments :: [a] -> [[[a]]]
+subsegments = tail . transpose . map inits . transpose . tails 
+
+{- |
+>>> mex [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6]
+15
+-}
+mex     ::  [Int] -> Int
+mex xs  =   minform 0 (length xs, xs)
+
+minform         ::  Int -> (Int, [Int]) -> Int
+minform a (n,xs)
+  | n == 0      =   a
+  | m == b - a  =   minform b (n-m, vs)
+  | otherwise   =   minform a (m, us)
+    where  (us,vs)  =  partition (< b) xs
+           b        =  a + 1 + n `div` 2
+           m        = length us
+
+{- misc -}
+toTuple :: [a] -> (a,a)
+toTuple = \ case
+    x:y:_ -> (x,y)
+    _     -> invalid
+
+fromTuple :: (a,a) -> [a]
+fromTuple (x,y) = [x,y]
+
+countif :: (a -> Bool) -> [a] -> Int
+countif = iter 0
+    where
+        iter a p (x:xs) = iter (bool a (succ a) (p x)) p xs
+        iter a _ []     = a
+
+{- error -}
+impossible :: a
+impossible = error "impossible"
+
+invalid :: a
+invalid = error "invalid input"
+
+{- debug -}
+trace :: String -> a -> a
+trace | debug     = Debug.trace
+      | otherwise = const id
+
+tracing :: Show a => a -> a
+tracing = trace . show <*> id
